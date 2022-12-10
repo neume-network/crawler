593672aebe4260ca730d67bc61b55b95ca969523
il3ven <vaibhavchanana@yahoo.co.in>
2022-12-10 17:00:41 +0530

    add more test cases for database module

--- database/index.test.ts
+++ database/index.test.ts
@@ -24,7 +24,7 @@ test.serial("should be able to get values by id", async (t) => {
   const datum = { chainId, address: "0x9b", tokenId: "2", blockNumber: "110" };
   const value = { test: "data" };
   await db.insert(datum, value);
-  const ret = await db.get(datum);
+  const ret = await db.getOne(datum);
   t.deepEqual(ret.id, datum);
   t.deepEqual(ret.value, value);
 });
@@ -44,15 +44,145 @@ test.serial(
     ];
     await Promise.all(values.map((v) => db.insert(v.id, v.value)));
     const { blockNumber, ...datum } = values[0].id;
-    const ret = await db.get(datum);
+    const ret = await db.getOne(datum);
     t.deepEqual(ret.id, values[1].id);
     t.deepEqual(ret.value, values[1].value);
   }
 );
 
-test.serial.skip(
-  "should get all tokenIds with the latest value if not block number is specied",
-  async (t) => {}
+test.serial(
+  "provided with only chainId and address should get all tokenIds",
+  async (t) => {
+    const address = "0xb8";
+    const values = [
+      {
+        id: { chainId, address, tokenId: "1", blockNumber: "110" },
+        value: { test: "data" },
+      },
+      {
+        id: { chainId, address, tokenId: "2", blockNumber: "110" },
+        value: { test: "data" },
+      },
+      {
+        id: { chainId, address, tokenId: "2", blockNumber: "120" },
+        value: { test: "updated-data" },
+      },
+      {
+        id: { chainId, address, tokenId: "3", blockNumber: "130" },
+        value: { test: "data" },
+      },
+      {
+        id: { chainId, address: "0xc8", tokenId: "3", blockNumber: "110" },
+        value: { test: "data" },
+      },
+    ];
+
+    await Promise.all(values.map((v) => db.insert(v.id, v.value)));
+    const ret = [];
+    for await (const r of db.getMany({ chainId, address })) {
+      ret.push(r);
+    }
+    t.deepEqual(ret, [values[0], values[2], values[3]]);
+  }
+);
+
+test.serial(
+  "provided with chainId, address and a block number should get all tokenIds upto the given block number",
+  async (t) => {
+    const address = "0xb8";
+    const values = [
+      {
+        id: { chainId, address, tokenId: "1", blockNumber: "110" },
+        value: { test: "data" },
+      },
+      {
+        id: { chainId, address, tokenId: "2", blockNumber: "110" },
+        value: { test: "data" },
+      },
+      {
+        id: { chainId, address, tokenId: "2", blockNumber: "120" },
+        value: { test: "updated-data" },
+      },
+      {
+        id: { chainId, address, tokenId: "3", blockNumber: "130" },
+        value: { test: "data" },
+      },
+      {
+        id: { chainId, address: "0xc8", tokenId: "3", blockNumber: "110" },
+        value: { test: "data" },
+      },
+    ];
+
+    await Promise.all(values.map((v) => db.insert(v.id, v.value)));
+    const ret = [];
+    for await (const r of db.getMany({
+      chainId,
+      address,
+      blockNumber: "110",
+    })) {
+      ret.push(r);
+    }
+    t.deepEqual(ret, [values[0], values[1]]);
+  }
+);
+
+test.serial("provided with only chainId should get all tokenIds", async (t) => {
+  const values = [
+    {
+      id: { chainId, address: "0xb8", tokenId: "1", blockNumber: "110" },
+      value: { test: "data" },
+    },
+    {
+      id: { chainId, address: "0xb9", tokenId: "2", blockNumber: "110" },
+      value: { test: "data" },
+    },
+    {
+      id: { chainId, address: "0xb9", tokenId: "2", blockNumber: "120" },
+      value: { test: "updated-data" },
+    },
+    {
+      id: { chainId, address: "0xc8", tokenId: "3", blockNumber: "130" },
+      value: { test: "data" },
+    },
+  ];
+
+  await Promise.all(values.map((v) => db.insert(v.id, v.value)));
+  const ret = [];
+  for await (const r of db.getMany({ chainId })) {
+    ret.push(r);
+  }
+  t.deepEqual(ret, [values[0], values[2], values[3]]);
+});
+
+test.serial(
+  "provided with chainId and blockNumber should get all tokenIds upto the given block number",
+  async (t) => {
+    const values = [
+      {
+        id: { chainId, address: "0xb8", tokenId: "1", blockNumber: "110" },
+        value: { test: "data" },
+      },
+      {
+        id: { chainId, address: "0xb9", tokenId: "2", blockNumber: "110" },
+        value: { test: "data" },
+      },
+      {
+        id: { chainId, address: "0xb9", tokenId: "2", blockNumber: "120" },
+        value: { test: "updated-data" },
+      },
+      {
+        id: { chainId, address: "0xc8", tokenId: "3", blockNumber: "130" },
+        value: { test: "data" },
+      },
+    ];
+
+    await Promise.all(values.map((v) => db.insert(v.id, v.value)));
+    const ret = [];
+    for await (const r of db.getMany({ chainId, blockNumber: "110" })) {
+      ret.push(r);
+    }
+    t.deepEqual(ret, [values[0], values[1]]);
+  }
 );
 
 test.serial("should rewrite data if id is same", async (t) => {
@@ -68,12 +198,15 @@ test.serial("should rewrite data if id is same", async (t) => {
   ];
   await Promise.all(values.map((v) => db.insert(v.id, v.value)));
   const { blockNumber, ...datum } = values[0].id;
-  const ret = await db.get(datum);
+  const ret = await db.getOne(datum);
   t.deepEqual(ret.id, values[0].id);
   t.deepEqual(ret.value, values[1].value);
 });
 
-test.after("clear database", async (t) => {
+test.afterEach("clear database", async (t) => {
   await db.level.clear();
+});
+
+test.after("close database", async (t) => {
   await db.level.close();
 });
---
+++ strategies/zora.ts
@@ -0,0 +1,104 @@
+// @ts-nocheck
+
+/**
+ * Note: The platform Catalog used to use Zora to mint music NFTs
+ * Since, Zora is a marketplace we will find non-music NFTs which
+ * we will have to filter after crawl.
+ */
+
+import { ExtractionWorkerHandler } from "@neume-network/extraction-worker";
+import { anyIpfsToNativeIpfs } from "ipfs-uri-utils";
+import { callTokenUri } from "../components/call-tokenuri.js";
+import { getIpfsTokenUri } from "../components/get-ipfs-tokenuri.js";
+import { NFT, Config } from "../types.js";
+
+import { Strategy } from "./strategy.types.js";
+
+export default class Zora implements Strategy {
+  public static version = "1.0.0";
+  public static createdAtBlock = 11565020;
+  public static deprecatedAtBlock = null;
+  private worker: ExtractionWorkerHandler;
+  private config: Config;
+
+  constructor(worker: ExtractionWorkerHandler, config: Config) {
+    this.worker = worker;
+    this.config = config;
+  }
+
+  async crawl(nft: NFT) {
+    nft = await callTokenUri(
+      this.worker,
+      this.config,
+      nft.erc721.createdAt,
+      nft
+    );
+
+    if (!nft.erc721.token.uri)
+      throw new Error(
+        `tokenURI shouldn't be empty ${JSON.stringify(nft, null, 2)}`
+      );
+
+    nft.erc721.token.uri = anyIpfsToNativeIpfs(nft.erc721.token.uri);
+
+    nft = await getIpfsTokenUri(nft);
+
+    if (!nft.erc721.token.uriContent)
+      throw new Error(
+        `tokenURI content shouldn't be empty ${JSON.stringify(nft, null, 2)}`
+      );
+
+    const datum = nft.erc721.token.uriContent;
+
+    const title = datum?.body?.title || datum?.name;
+    const artist = datum?.body?.artist;
+    const description = datum?.body?.notes;
+    const artwork = datum?.body?.artwork?.info?.uri;
+    let duration;
+    if (datum.body && datum.body.duration) {
+      duration = `PT${Math.floor(datum.body.duration / 60)}M${(
+        datum.body.duration % 60
+      ).toFixed(0)}S`;
+    }
+
+    return JSON.stringify({
+      version,
+      title,
+      duration,
+      artist: {
+        version,
+        name: artist,
+      },
+      platform: {
+        version,
+        name: "Catalog",
+        uri: "https://beta.catalog.works",
+      },
+      erc721: {
+        version,
+        // TODO
+        //address: nft[1],
+        //tokenId: nft[2],
+        tokenURI: metadata.tokenURI,
+        metadata: {
+          ...datum,
+          name: title,
+          description,
+        },
+      },
+      manifestations: [
+        {
+          version,
+          // TODO: Zora's file URL can be retrieved when calling tokenURI
+          //uri: "https://example.com/file",
+          //mimetype: datum.body.mimeType,
+        },
+        {
+          version,
+          uri: artwork,
+          mimetype: "image",
+        },
+      ],
+    });
+  }
+}
