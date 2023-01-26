bed649006bc5563489810166cd820e78aab87ec4
il3ven <vaibhavchanana@yahoo.co.in>
2023-01-27 02:12:54 +0530

    run `npm run build`

--- dist/src/strategies/sound.js
+++ dist/src/strategies/sound.js
@@ -1,12 +1,16 @@
+/**
+ * Sound is the predecessor to sound-protocol
+ */
 import { decodeLog, toHex } from "eth-fun";
 import { callTokenUri } from "../components/call-tokenuri.js";
-import { getArweaveTokenUri } from "../components/get-arweave-tokenuri.js";
+import { fetchTokenUri } from "../components/fetch-tokenuri.js";
 import { callOwner } from "../components/call-owner.js";
 import { randomItem } from "../utils.js";
-export default class SoundProtocol {
+import { ifIpfsConvertToNativeIpfs } from "ipfs-uri-utils";
+export default class Sound {
     constructor(worker, config) {
         this.filterContracts = async (from, to) => {
-            const editionCreatedSelector = "0x405098db99342b699216d8150e930dbbf2f686f5a43485aed1e69219dafd4935";
+            const artistCreatedSelector = "0x23748b43b77f98380e738976c6324996908ffc1989994dd3c68631c87a65a7c0";
             const rpcHost = randomItem(this.config.rpc);
             const options = {
                 url: rpcHost.url,
@@ -22,63 +26,84 @@ export default class SoundProtocol {
             const message = await this.worker({
                 type: "json-rpc",
                 method: "eth_getLogs",
-                commissioner: SoundProtocol.name,
+                commissioner: Sound.name,
                 params: [
                     {
                         fromBlock,
                         toBlock,
-                        topics: [[editionCreatedSelector]],
+                        topics: [[artistCreatedSelector]],
                     },
                 ],
                 version: "0.0.1",
                 options,
             });
             if (message.error) {
-                throw new Error(`Error occured while filtering ${SoundProtocol.name} contracts: \n${JSON.stringify(message, null, 2)}`);
+                throw new Error(`Error occured while filtering ${Sound.name} contracts: \n${JSON.stringify(message, null, 2)}`);
             }
             const logs = message.results;
-            return logs.map((log) => {
+            const contracts = logs.map((log) => {
                 const topics = log.topics;
                 topics.shift();
                 const result = decodeLog([
-                    { type: "address", name: "soundEdition", indexed: true },
-                    { type: "address", name: "deployer", indexed: true },
-                    { type: "bytes", name: "initData" },
-                    { type: "address[]", name: "contracts" },
-                    { type: "bytes[]", name: "data" },
-                    { type: "bytes[]", name: "results" },
+                    {
+                        type: "uint256",
+                        name: "artistId",
+                    },
+                    {
+                        type: "string",
+                        name: "name",
+                    },
+                    {
+                        type: "string",
+                        name: "symbol",
+                    },
+                    {
+                        type: "address",
+                        name: "artistAddress",
+                        indexed: true,
+                    },
                 ], log.data, topics);
                 return {
-                    address: result.soundEdition.toLowerCase(),
-                    name: SoundProtocol.name,
-                    version: SoundProtocol.version,
+                    address: result.artistAddress.toLowerCase(),
+                    name: Sound.name,
+                    version: Sound.version,
                 };
             });
+            return contracts;
         };
         this.crawl = async (nft) => {
-            if (SoundProtocol.invalidIDs.filter((id) => `${nft.erc721.address}/${nft.erc721.token.id}`.match(id)).length != 0) {
+            // Instead of querying at the block number soundxyz NFT
+            // was minted, we query at a higher block number because
+            // soundxyz changed their tokenURI and the previous one
+            // doesn't work anymore. https://github.com/neume-network/data/issues/19
+            //
+            // createdAtBlock           13725566 (https://etherscan.io/tx/0xfa325f74eb6c7f5e6bb60a264404543d6158f79de01bc5aab35180354e554dce)
+            // workingAfterBlockNumber  15050010
+            const WORKING_AFTER_BLOCK = 15050010;
+            if (Sound.invalidIDs.filter((id) => `${nft.erc721.address}/${nft.erc721.token.id}`.match(id))
+                .length != 0) {
                 console.log(`Ignoring ${nft.erc721.address}/${nft.erc721.token.id} because it is blacklisted`);
                 return null;
             }
-            nft.erc721.token.uri = await callTokenUri(this.worker, this.config, nft.erc721.blockNumber, nft);
-            nft.erc721.token.uriContent = await getArweaveTokenUri(nft.erc721.token.uri, this.worker, this.config);
+            nft.erc721.token.uri = await callTokenUri(this.worker, this.config, Math.max(nft.erc721.blockNumber, WORKING_AFTER_BLOCK), nft);
+            nft.erc721.token.uriContent = await fetchTokenUri(nft.erc721.token.uri, this.worker);
             nft.creator = await callOwner(this.worker, this.config, nft.erc721.address, nft.erc721.blockNumber);
             const datum = nft.erc721.token.uriContent;
             return {
-                version: SoundProtocol.version,
+                version: Sound.version,
                 title: datum.name,
                 artist: {
-                    version: SoundProtocol.version,
-                    name: datum.artist,
+                    version: Sound.version,
+                    name: datum.artist_name,
                     address: nft.creator,
                 },
                 platform: {
-                    version: SoundProtocol.version,
-                    name: "Sound Protocol",
+                    version: Sound.version,
+                    name: "Sound",
                     uri: "https://sound.xyz",
                 },
                 erc721: {
-                    version: SoundProtocol.version,
+                    version: Sound.version,
                     createdAt: nft.erc721.blockNumber,
                     transaction: {
                         from: nft.erc721.transaction.from,
@@ -98,13 +123,18 @@ export default class SoundProtocol {
                 },
                 manifestations: [
                     {
-                        version: SoundProtocol.version,
-                        uri: datum.losslessAudio,
+                        version: Sound.version,
+                        uri: ifIpfsConvertToNativeIpfs(datum.audio_url),
                         mimetype: "audio",
                     },
                     {
-                        version: SoundProtocol.version,
-                        uri: datum.image,
+                        version: Sound.version,
+                        uri: ifIpfsConvertToNativeIpfs(datum.image),
+                        mimetype: "image",
+                    },
+                    {
+                        version: Sound.version,
+                        uri: ifIpfsConvertToNativeIpfs(datum.animation_url),
                         mimetype: "image",
                     },
                 ],
@@ -115,10 +145,7 @@ export default class SoundProtocol {
     }
     updateOwner(nft) { }
 }
-SoundProtocol.version = "1.0.0";
-SoundProtocol.createdAtBlock = 15570834;
-SoundProtocol.deprecatedAtBlock = null;
-SoundProtocol.invalidIDs = [
-    /^0xdf4f25cd13567a74572063dcf15f101c22be1af0\/321$/,
-    /^0x9f396644ec4b2a2bc3c6cf665d29165dde0e83f1\/\d+$/, // tokenURI is not properly formatted and is not Arweave
-];
+Sound.version = "1.0.0";
+Sound.createdAtBlock = 13725566;
+Sound.deprecatedAtBlock = null;
+Sound.invalidIDs = [];
