e7403cb21a46f425c43e484a28fb188eda6a12ce
il3ven <vaibhavchanana@yahoo.co.in>
2023-02-02 17:24:30 +0530

    run npm run build

--- dist/commands/init.js
+++ dist/commands/init.js
@@ -3,15 +3,12 @@ import fs from "fs/promises";
 import { saveLastCrawledBlock } from "../src/state.js";
 import { CONSTANTS } from "../src/types.js";
 export default async function init() {
-  await fs.copyFile(new URL("../assets/.env-copy", import.meta.url), path.resolve(".env"));
-  await fs.copyFile(
-    new URL("../assets/config.sample.js", import.meta.url),
-    path.resolve("./config.js"),
-  );
-  // Will create file if it does not exist
-  await fs.writeFile(path.resolve("./data/contracts.json"), "{}", {
-    flag: "w",
-  });
-  // Create the last_crawled_block file in ./data
-  await saveLastCrawledBlock(CONSTANTS.FIRST_BLOCK);
+    await fs.copyFile(new URL("../assets/.env-copy", import.meta.url), path.resolve(".env"));
+    await fs.copyFile(new URL("../assets/config.sample.js", import.meta.url), path.resolve("./config.js"));
+    // Will create file if it does not exist
+    await fs.writeFile(path.resolve("./data/contracts.json"), "{}", {
+        flag: "w",
+    });
+    // Create the last_crawled_block file in ./data
+    await saveLastCrawledBlock(CONSTANTS.FIRST_BLOCK);
 }
--- dist/commands/sync.js
+++ dist/commands/sync.js
@@ -5,56 +5,51 @@ import { db } from "../database/index.js";
 import path from "path";
 import { getUserContracts } from "../src/utils.js";
 async function getLastSyncedBlock() {
-  const lastId = await db.changeIndex.iterator({ reverse: true, limit: 1 }).next();
-  return lastId ? parseInt(lastId[0].split("/")[0]) : 15000000;
+    const lastId = await db.changeIndex.iterator({ reverse: true, limit: 1 }).next();
+    return lastId ? parseInt(lastId[0].split("/")[0]) : 15000000;
 }
 export default async function (from, to, url, config) {
-  const worker = ExtractionWorker(config.worker);
-  let client;
-  let id = 0;
-  client = new JSONRPCClient(
-    (jsonRPCRequest) =>
-      worker({
+    const worker = ExtractionWorker(config.worker);
+    let client;
+    let id = 0;
+    client = new JSONRPCClient((jsonRPCRequest) => worker({
         type: "https",
         version: "0.0.1",
         commissioner: "",
         options: {
-          url,
-          method: "POST",
-          headers: {
-            "content-type": "application/json",
-          },
-          body: JSON.stringify(jsonRPCRequest),
-          retry: {
-            retries: 3,
-          },
+            url,
+            method: "POST",
+            headers: {
+                "content-type": "application/json",
+            },
+            body: JSON.stringify(jsonRPCRequest),
+            retry: {
+                retries: 3,
+            },
         },
-      }).then((msg) => {
-        if (msg.error) return Promise.reject(new Error(JSON.stringify(msg.error)));
+    }).then((msg) => {
+        if (msg.error)
+            return Promise.reject(new Error(JSON.stringify(msg.error)));
         return client.receive(msg.results);
-      }),
-    () => (++id).toString(), // HACK because of a bug in JSON-RPC-Client
-  );
-  let syncFrom = from ?? (await getLastSyncedBlock());
-  console.log("Will sync from", syncFrom, "to", to);
-  // Sync contracts
-  const userContractsNew = await client.request("getUserContracts", null);
-  const userContractsOld = await getUserContracts();
-  const userContracts = { ...userContractsNew, ...userContractsOld };
-  const userContractsPath = path.resolve("./data/contracts.json");
-  await fs.writeFile(userContractsPath, JSON.stringify(userContracts, null, 2));
-  console.log("Updated local list of contracts");
-  for (let syncedTill = syncFrom; syncedTill <= to; syncedTill += 5000) {
-    console.log(`Syncing from ${syncedTill} to ${syncedTill + 5000}`);
-    const returnValues = await client.request("getIdsChanged_fill", [
-      syncedTill.toString(),
-      (syncedTill + 5000).toString(),
-    ]);
-    await Promise.all(
-      returnValues.map(async (r) => {
-        await db.insert(r.id, r.value);
-      }),
-    );
-    console.log(`Wrote ${returnValues.length} entries to database`);
-  }
+    }), () => (++id).toString());
+    let syncFrom = from ?? (await getLastSyncedBlock());
+    console.log("Will sync from", syncFrom, "to", to);
+    // Sync contracts
+    const userContractsNew = await client.request("getUserContracts", null);
+    const userContractsOld = await getUserContracts();
+    const userContracts = { ...userContractsNew, ...userContractsOld };
+    const userContractsPath = path.resolve("./data/contracts.json");
+    await fs.writeFile(userContractsPath, JSON.stringify(userContracts, null, 2));
+    console.log("Updated local list of contracts");
+    for (let syncedTill = syncFrom; syncedTill <= to; syncedTill += 5000) {
+        console.log(`Syncing from ${syncedTill} to ${syncedTill + 5000}`);
+        const returnValues = (await client.request("getIdsChanged_fill", [
+            syncedTill.toString(),
+            (syncedTill + 5000).toString(),
+        ]));
+        await Promise.all(returnValues.map(async (r) => {
+            await db.insert(r.id, r.value);
+        }));
+        console.log(`Wrote ${returnValues.length} entries to database`);
+    }
 }
